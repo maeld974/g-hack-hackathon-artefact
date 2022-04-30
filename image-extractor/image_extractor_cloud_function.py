46c98ac0fed8a7aeb0d789b25dccf4ab9544e559
pablo <pablovizan@gmail.com>
2022-04-30 17:48:13 +0200

    new cloud function modified

--- image-extractor/image_extractor_cloud_function.py
+++ image-extractor/image_extractor_cloud_function.py
@@ -7,21 +7,32 @@ import csv
 from google.oauth2 import service_account
 from google.cloud import vision
 from google.cloud import storage
-# from wand.image import Image
 
 storage_client = storage.Client()
 
-def return_labels(file_data):
-	
-    file_name = file_data['name']
-    bucket_name = file_data['bucket']
+def main(request):
+	"""Responds to any HTTP request.
+	Args:
+		request (flask.Request): HTTP request object.
+	Returns:
+		The response text or any set of values that can be turned into a
+		Response object using
+		`make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+	"""
+	request_json = request.get_json()
 
-    blob = storage_client.bucket(bucket_name).get_blob(file_name)
-    file_name = blob.name
+	file_name = request_json['file_name']
+	bucket_name = 'input_images_ghack2022'
 
-    # Loads the image into memory
+	blob = storage_client.bucket(bucket_name).get_blob(file_name)
+	file_name = blob.name
+
+	credentials = service_account.Credentials.from_service_account_file('gcloud_key.json')
+	client = vision.ImageAnnotatorClient(credentials=credentials)
+
+	# Loads the image into memory
 	with io.open(file_name, 'rb') as image_file:
-	    content = image_file.read()
+		content = image_file.read()
 
 	# Run the image through google vision API
 	image = vision.Image(content=content)
@@ -44,54 +55,5 @@ def return_labels(file_data):
 		for label in labels:
 			writer.writerow([label.description, label.score])
 
-# def return_labels(data, context):
-# 	file_data = data
-
-#     file_name = file_data['name']
-#     bucket_name = file_data['bucket']
-
-#     blob = storage_client.bucket(bucket_name).get_blob(file_name)
-#     file_name = blob.name
-#     _, temp_local_filename = tempfile.mkstemp()
-
-#     # Download file from bucket.
-#     current_blob.download_to_filename(temp_local_filename)
-#     print(f'Image {file_name} was downloaded to {temp_local_filename}.')
-
-#     # Load credentials 
-# 	credentials = service_account.Credentials.from_service_account_file('gcloud_key.json')
-
-# 	# Create image annotator client
-# 	client = vision.ImageAnnotatorClient(credentials=credentials)
-
-# 	# Input the name of the image file to annotate
-# 	input_image = 'sample_images/image_1.jpg'
-# 	file_name = os.path.abspath(temp_local_filename)
-
-# 	# Loads the image into memory
-# 	with io.open(file_name, 'rb') as image_file:
-# 	    content = image_file.read()
-
-# 	# Run the image through google vision API
-# 	image = vision.Image(content=content)
-
-# 	# Perform label detection on the image file
-# 	response = client.label_detection(image=image)
-# 	labels = response.label_annotations
-
-# 	# Get all 
-
-# 	# Create CSV file with output labels
-# 	with open('labels.csv', 'w') as file:
-# 		header = ['label', 'score']
-
-# 		# CSV writer
-# 		writer = csv.writer(file)
-
-# 		# Write header
-# 		writer.writerow(header)
-
-# 		# For label extracted with the API, create a new row in the outputs datset
-# 		for label in labels:
-# 			writer.writerow([label.description, label.score])
-
+	return 'OK'
+	
